73fb949862fc8c26c1c0407a4e6eac1a3c49085f
Rémi Lefort <rlefort@eunomart.com>
2026-03-04 13:00:13 +0100

    add Intercom adapter (#1)
    * add Intercom adapter

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 var CONFIG = require("./config");
 var CrispImport = require("./lib/import");
 
-var Import = new CrispImport({
-  websiteId: CONFIG.WEBSITE_ID,
-  websitePlan: CONFIG.WEBSITE_PLAN,
-  urn: CONFIG.PLUGIN_URN,
-  name: CONFIG.PLUGIN_NAME,
-  identifier: CONFIG.PLUGIN_TOKEN_IDENTIFIER,
-  key: CONFIG.PLUGIN_TOKEN_KEY,
-  defaultUserEmail: CONFIG.DEFAULT_USER_EMAIL,
-  defaultUserNickname: CONFIG.DEFAULT_USER_NICKNAME,
-  defaultOperatorNickname: CONFIG.DEFAULT_OPERATOR_NICKNAME
-});
+var Import = new CrispImport(
+  {
+    websiteId: CONFIG.WEBSITE_ID,
+    websitePlan: CONFIG.WEBSITE_PLAN,
+    urn: CONFIG.PLUGIN_URN,
+    name: CONFIG.PLUGIN_NAME,
+    identifier: CONFIG.PLUGIN_TOKEN_IDENTIFIER,
+    key: CONFIG.PLUGIN_TOKEN_KEY,
+    defaultUserEmail: CONFIG.DEFAULT_USER_EMAIL,
+    defaultUserNickname: CONFIG.DEFAULT_USER_NICKNAME,
+    defaultOperatorNickname: CONFIG.DEFAULT_OPERATOR_NICKNAME
+  },
+  {
+    adapter : "intercom",
+    resume  : false   // set to true to resume an interrupted import
+  }
+);
 
 Import.importFromFile("./res/conversations.json")
   .then((result) => {
